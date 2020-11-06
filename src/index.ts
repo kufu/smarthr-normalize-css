27cf2884f2a314760183adaa1bcd6f79ce561cbd
cidermitaina <cidermitaina@gmail.com>
2020-11-06 10:34:04 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { css, createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+export const cssBaseLine = css`
+  ${reset}
+
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    word-wrap: break-word;
+    font-family: system-ui, sans-serif;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  img {
+    vertical-align: middle;
+  }
+  input, button, textarea {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    border: none;
+    background-color: inherit;
+    color: inherit;
+  }
+`
+
+export const CssBaseLine = createGlobalStyle`${cssBaseLine}`
+
+export default cssBaseLine
