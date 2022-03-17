786c7174ea3d0d43f4fdc8cfe81f8d4390bc3eaf
Soichi Masuda <masuP9@gmail.com>
2022-03-17 18:39:17 +0900

    Improve accessibility (#47)
    * feat: font-size as browser default
    
    * refactor: use overflow-wrap instead of word-wrap
    
    * feat: link has underline by default
    
    * feat: make appear focus indicator by default

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,24 @@ import reset from 'styled-reset'
 export const cssBaseLine = css`
   ${reset}
 
-  html {
-    font-size: 62.5%;
-  }
   body {
-    word-wrap: break-word;
+    overflow-wrap: break-word;
     font-family: system-ui, sans-serif;
   }
+
   a {
     color: inherit;
-    text-decoration: none;
   }
+
   img {
     vertical-align: middle;
   }
+
   input,
   button,
   textarea {
     margin: 0;
     padding: 0;
-    outline: none;
     border: none;
     background-color: inherit;
     color: inherit;
